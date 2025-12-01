68122e175cab8bda65143ccfae73f9ff26f2f38c
Alexey Yumanov <inactive0073@gmail.com>
2025-12-01 10:20:49 +0400

    added new fixtures

--- conftest.py
+++ conftest.py
@@ -29,19 +29,21 @@ def auth_headers():
 
 
 @pytest.fixture(scope="function")
-def post(api_client):
+def post(api_client, post_dao: PostDAO):
     """Создаёт тестовую сущность перед каждым тестом и удаляет после."""
     actions = PostActions(api_client)
     post = actions.create_post(PostCreationRequest.random())
     yield post
+    post_dao.delete(post.id)
 
 
 @pytest.fixture(scope="function")
-def user(api_client):
+def user(api_client, user_dao: UserDAO):
     """Создаёт тестовую сущность перед каждым тестом и удаляет после."""
     actions = UserActions(api_client)
     user = actions.create_user(UserCreationRequest.random())
     yield user
+    user_dao.delete(user.id)
 
 
 @pytest.fixture(scope="session")
@@ -54,18 +56,38 @@ def user_actions(api_client):
     return UserActions(api_client)
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def db():
     client = DBClient(db_config)
     yield client
     client.close()
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def post_dao(db):
     return PostDAO(db)
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def user_dao(db):
     return UserDAO(db)
+
+
+@pytest.fixture(scope="function")
+def user_in_db(
+    user_dao: UserDAO,
+) -> Generator[tuple[UserCreationRequest, int], None, None]:
+    req = UserCreationRequest.random()
+    user_id = user_dao.create_user(req)
+    yield req, user_id
+    user_dao.delete(user_id)
+
+
+@pytest.fixture(scope="function")
+def post_in_db(
+    post_dao: PostDAO,
+) -> Generator[tuple[PostCreationRequest, int], None, None]:
+    req = PostCreationRequest.random()
+    post_id = post_dao.create_post_direct(req)
+    yield req, post_id
+    post_dao.delete(post_id)
