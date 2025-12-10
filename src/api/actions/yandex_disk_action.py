2a790793b83a06e661adc197005e3c27342dcf46
Alexey Yumanov <inactive0073@gmail.com>
2025-12-11 01:07:50 +0400

    added new action for yd crud operations

--- src/api/actions/yandex_disk_action.py
+++ src/api/actions/yandex_disk_action.py
@@ -1,3 +1,4 @@
+from typing import Literal
 import allure
 from requests import Response
 
@@ -19,3 +20,41 @@ class DiskActions:
     def get_disk_info_without_auth(self) -> Response:
         # без токена
         return self.client.get(YandexEndpoints.DISK_INFO)
+
+    @allure.step("Создание папки на Яндекс Диске")
+    def create_folder(self, folder_path: str) -> Response:
+        params = {"path": folder_path}
+        return self.client.put(YandexEndpoints.RESOURCES, params=params)
+
+    @allure.step("Получение информации о файле или каталоге")
+    def get_resource_info(self, resource_path: str) -> Response:
+        params = {"path": resource_path}
+        return self.client.get(YandexEndpoints.RESOURCES, params=params)
+
+    @allure.step("Получение списка удалённых элементов на Яндекс Диске")
+    def get_trash_items(self) -> Response:
+        return self.client.get(YandexEndpoints.GET_TRASH_ITEMS)
+
+    @allure.step("Удаление папки на Яндекс Диске")
+    def delete_folder(
+        self, folder_path: str, permanently: Literal["true", "false"] = "false"
+    ) -> Response:
+        params = {"path": folder_path, "permanently": permanently}
+        return self.client.delete(YandexEndpoints.RESOURCES, params=params)
+
+    @allure.step("Восстановление удалённого элемента на Яндекс Диске")
+    def restore_trash_item(self, item_path: str) -> Response:
+        params = {"path": item_path}
+        return self.client.post(YandexEndpoints.RESTORE_TRASH_ITEM, params=params)
+
+    @allure.step("Получение ссылки для загрузки файла на Яндекс Диск")
+    def get_upload_link(
+        self, file_path: str, overwrite: Literal["true", "false"] = "true"
+    ) -> Response:
+        params = {"path": file_path, "overwrite": overwrite}
+        return self.client.get(YandexEndpoints.UPLOAD, params=params)
+
+    @allure.step("Загрузка файла на Яндекс Диск по ссылке")
+    def upload_file(self, upload_url: str, file_data: bytes) -> Response:
+        headers = {"Content-Type": "application/octet-stream"}
+        return self.client.post(upload_url, data=file_data, headers=headers)
