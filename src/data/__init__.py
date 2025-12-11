fc9eeb27edceb80cea966561946d8efe1f7d5eba
Alexey Yumanov <inactive0073@gmail.com>
2025-12-11 14:57:05 +0400

    fix data generator from time() to uuid4()

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,4 +1,4 @@
-from .wp_endpoints import Endpoints
 from .data_generator import generate_random_folder_name
+from .wp_endpoints import Endpoints
 
 __all__ = ["Endpoints", "generate_random_folder_name"]
--- src/data/data_generator.py
+++ src/data/data_generator.py
@@ -1,7 +1,7 @@
-import time
+from uuid import uuid4
 
 
 def generate_random_folder_name() -> str:
     """Генерирует случайное имя папки для тестов."""
-    timestamp = int(time.time())
-    return f"test_folder_{timestamp}"
+    unique_id = str(uuid4())[:8]
+    return f"test_folder_{unique_id}"
