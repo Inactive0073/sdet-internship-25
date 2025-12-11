1bd638d327998ea990a27f9dfb60453beac2a1f2
Alexey Yumanov <inactive0073@gmail.com>
2025-12-11 14:55:23 +0400

    added bat for launch scripts

---
+++ scripts/run_tests_single.cmd
@@ -0,0 +1,59 @@
+@echo off
+chcp 65001 >nul
+
+set "VENV_DIR=.venv"
+
+echo Running single test...
+
+python -m pip install --upgrade pip
+
+if not exist %VENV_DIR% (
+    python -m venv %VENV_DIR%
+    if %ERRORLEVEL% NEQ 0 goto failed_venv
+)
+
+call %VENV_DIR%\Scripts\activate.bat
+if %ERRORLEVEL% NEQ 0 goto failed_activate
+
+pip install -r requirements.txt
+if %ERRORLEVEL% NEQ 0 goto failed_dependencies
+
+pytest -v -n 0 --alluredir=allure-results -m yandex_disk
+if %ERRORLEVEL% NEQ 0 goto failed_tests
+
+:: --- Успешное завершение ---
+echo.
+echo =================================
+echo Тесты успешно завершены!
+echo =================================
+goto end
+
+:: --- Обработка ошибок (Фолбэки) ---
+:failed_activate
+echo.
+echo Ошибка: Не удалось активировать виртуальное окружение.
+goto error_exit
+
+:failed_dependencies
+echo.
+echo Ошибка: Не удалось установить зависимости.
+goto error_exit
+
+:failed_venv
+echo.
+echo Ошибка: Не удалось создать виртуальное окружение.
+goto error_exit
+
+:failed_tests
+echo.
+echo Ошибка: Тесты завершились неудачно.
+goto error_exit
+
+:error_exit
+echo.
+pause
+exit /b %ERRORLEVEL%
+
+:end
+echo.
+pause
