7e0e00ae907b9a4dc31c9d9fe8c24e1a811f318f
Alexey Yumanov <inactive0073@gmail.com>
2025-12-15 22:30:40 +0400

    added new endpoints

--- src/data/yandex_endpoints.py
+++ src/data/yandex_endpoints.py
@@ -8,6 +8,7 @@ class YandexEndpoints:
     UPLOAD = "/v1/disk/resources/upload"
     DOWNLOAD = "/v1/disk/resources/download"
     COPY = "/v1/disk/resources/copy"
+    FILES_LIST = "/v1/disk/resources/files"
 
     GET_TRASH_ITEMS = "/v1/disk/trash/resources"
     RESTORE_TRASH_ITEM = "/v1/disk/trash/resources/restore"
