74eaec9ae8416f90da8a9c8983d2e486c088ed29
Alexey Yumanov <inactive0073@gmail.com>
2025-12-15 21:02:49 +0400

    fix compare func

--- src/utils/compare_two_files.py
+++ src/utils/compare_two_files.py
@@ -1,7 +1,19 @@
+import io
+import zipfile
+
+
+def extract_file_from_zip(
+    zip_bytes: bytes, file_name: str
+) -> bytes:
+    with zipfile.ZipFile(io.BytesIO(zip_bytes)) as zip_file:
+        with zip_file.open(file_name) as extracted:
+            return extracted.read()
+
+
 def compare_texts(
-    file_path: str, downloaded_content: bytes, encoding: str = "utf-8"
+    file_path: str, downloaded_content: bytes, zip_file_path: str = "sdet_data/data.txt"
 ) -> bool:
     """Вспомогательная функция для сравнения контента и файла"""
     with open(file_path, "rb") as f:
-        original = f.read()  # тоже bytes!
-    return downloaded_content == original
+        original = f.read()
+    return extract_file_from_zip(downloaded_content, zip_file_path) == original
